39d74898ce0d96a3b9eea7274bf7d4fecabef603
Vinicius <naoprefiro@gmail.com>
2023-03-08 01:00:58 -0300

    projeto finalizado

---
+++ nossosClientes.js
@@ -0,0 +1,5 @@
+const nossoClientes = document.querySelectorAll("logos-nossosClientes");
+console.log(nossoClientes)
+const imagemCliente = document.createElement("img")
+imagemCliente.src = "https://www.kasandbox.org/programming-images/animals/cat.png";
+nossoClientes.appendChild(imagemCliente);
